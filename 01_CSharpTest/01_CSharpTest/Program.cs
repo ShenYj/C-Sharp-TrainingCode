b98c4b94027e9ed653431b41bf2ba07969c7d795
ShenYj <shenyj4@51nb.com>
2017-11-29 22:20:52 +0800

    C# 占位符

--- 01_CSharpTest/01_CSharpTest/Program.cs
+++ 01_CSharpTest/01_CSharpTest/Program.cs
@@ -6,21 +6,35 @@ namespace _CSharpTest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello C Sharp !");
+            
+            Console.WriteLine("请输入您的姓名: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("请输入您的年龄:");
+            string age = Console.ReadLine();
+            Console.WriteLine("请输入您的性别:");
+            string sex = Console.ReadLine();
 
-            int a = 10;
-            int b = 23;
+            Console.WriteLine("确认您的信息:\n 姓名:{0}\n 年龄:{1} \n 性别:{2}",name,age,sex);
 
-            int result = calculate(a, b);
-            int[] array = { 2, 256, 6543, 12 };
-            result = sum(array);
-            Console.WriteLine(result);
+        }
+
+        public static void test ()
+        {
+            //Console.WriteLine("Hello C Sharp !");
+
+            //int a = 10;
+            //int b = 23;
+
+            //int result = calculate(a, b);
+            //int[] array = { 2, 256, 6543, 12 };
+            //result = sum(array);
+            //Console.WriteLine(result);
 
 
-            string name = "小桌子";
-            int age = 18;
+            //string name = "小桌子";
+            //int age = 18;
 
-            Console.WriteLine("姓名: " + name + ";\n" + "年龄: " + age);
+            //Console.WriteLine("姓名: " + name + ";\n" + "年龄: " + age);
         }
 
         /// <summary>
