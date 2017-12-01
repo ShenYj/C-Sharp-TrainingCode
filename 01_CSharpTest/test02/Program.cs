6cdadbe70ed0bbd5fddf744aa8cef321fb817d6d
ShenYj <shenyj4@51nb.com>
2017-12-01 17:41:31 +0800

    none

---
+++ 01_CSharpTest/test02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
