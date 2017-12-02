2aac39555fbc129fadc0f17999e59929c5d3cb3c
UNA-20171114VZR\Ryan Shen <shenyj4@51nb.com>
2017-12-02 14:37:54 +0800

    no message

---
+++ 01_CSharpTest/test02/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test02
+{
+    class Person
+    {
+        public string _name;
+        public int _age;
+        public char _gender;
+
+        public void Eat ()
+        {
+            Console.WriteLine("吃");
+        }
+
+        public void Drink ()
+        {
+            Console.WriteLine("喝");
+        }
+
+        public static void descript()
+        {
+            Console.WriteLine("~~~~~静态方法");
+        }
+    }
+}
--- 01_CSharpTest/test02/Program.cs
+++ 01_CSharpTest/test02/Program.cs
@@ -7,6 +7,25 @@ namespace test02
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Person wangmazi = new Person();
+
+            wangmazi._name = "王麻子";
+            wangmazi._age = 25;
+            wangmazi._gender = '男';
+
+            Person.descript();
+            
+
+            wangmazi.Eat();
+            wangmazi.Drink();
+
+            Console.ReadKey();
         }
     }
 }
+
+
+
+
+
