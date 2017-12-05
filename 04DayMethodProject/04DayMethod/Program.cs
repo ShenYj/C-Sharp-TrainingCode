8bcfbcce46802e8b2ec229eb3db50175e4dddf5f
Ryan Shen <shenyj4@51nb.com>
2017-12-05 17:58:17 +0800

    方法

---
+++ 04DayMethodProject/04DayMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// C# 方法练习
+/// </summary>
+namespace _04DayMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 123;
+            int number2 = 42;
+
+            int result = GetMaxNumber(123, 42);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static int GetMaxNumber(int n1, int n2)
+        {
+            return (n1 > n2) ? n1 : n2;
+        }
+    }
+}
