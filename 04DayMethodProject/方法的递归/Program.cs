af1b9348453293fabbb9d481476535db23428c97
Ryan Shen <shenyj4@51nb.com>
2017-12-06 18:22:17 +0800

    递归

---
+++ 04DayMethodProject/方法的递归/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 方法的递归
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TellStory(0);
+            Console.ReadKey();
+        }
+
+        // 方法的递归
+        public static void TellStory(int i)
+        {
+            Console.WriteLine("老和尚讲故事");
+            i++;
+            if (i >= 10) return;
+
+            TellStory(i);
+        }
+
+    }
+}
---
+++ 04DayMethodProject/方法重载/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 方法重载
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum1 = Sum(12, 54);
+            double sum2 = Sum(12.2, 23.5);
+            
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.ReadKey();
+
+        }
+
+
+        // 方法重载  方法的重载跟返回值没有关系
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static float Sum(float a, float b)
+        {
+            return a + b;
+        }
+
+    }
+}
