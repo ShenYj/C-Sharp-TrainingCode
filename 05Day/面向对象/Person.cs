1de0e99faf247b3326d3ab1ac72e559b1b089dde
Ryan Shen <shenyj4@51nb.com>
2017-12-08 15:16:16 +0800

    类，对象，属性，方法，字段，构造函数练习

---
+++ 05Day/面向对象/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象
+{
+    public class Person
+    {
+        // 1.字段
+        public string _name;
+        public int _age;
+        public char _gender;
+        // 2.属性 （作用：保护字段， 对字段的取值/赋值 进行限定）
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value < 0 || value > 100)
+                {
+                    value = 0;
+                }
+                _name = value;
+            }
+        }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+
+        // 3.1方法 (静态方法)
+        public static void Description()
+        {
+            Console.WriteLine("Person 类的静态方法");
+        }
+            
+        // 3.2方法 (非静态方法)
+        public void Eat(string sth)
+        {
+            Console.WriteLine("{0} 吃 {1}",this._name,sth);
+        }
+        public void Drink(string sth)
+        {
+            Console.WriteLine("{0} 喝 {1}",this._name,sth);
+        }
+    }
+}
---
+++ 05Day/面向对象/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 面向对象
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.Description(); // 调用静态方法
+
+            Person xiaoMing = new Person();
+            xiaoMing.Name = "小明";
+            //xiaoMing._name = "小明";
+            
+            xiaoMing.Eat("炸鸡");
+            xiaoMing.Drink("啤酒");
+            Console.ReadKey();
+        }
+    }
+}
