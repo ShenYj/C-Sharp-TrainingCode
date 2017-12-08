aafc5d2d1107e5341dcd11925b3b3516890af418
Ryan Shen <shenyj4@51nb.com>
2017-12-08 20:14:01 +0800

    字符串

---
+++ 05Day/StringBuilder/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace StringBuilder练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // StringBuilder 练习
+            //Test1();  18s左右， 原因：频繁的开辟内存空间
+            //Test2();  // 不到1s
+
+            // 转大写、转小写
+            string c = "c";
+            string b = "B";
+
+            Console.WriteLine(c.ToUpper());// 转大写
+            Console.WriteLine(b.ToLower());// 转小写
+
+            string str1 = "c#";
+            string str2 = "C#";
+
+            // 忽略大小写
+            if (str1.Equals(str2, StringComparison.OrdinalIgnoreCase)) Console.WriteLine("相同");
+
+            // 分割字符串
+            string s = "a b dfd _ + = ,, fasdfasd";
+            Console.WriteLine(s);
+            // 将不需要的字符换成空
+            char[] chs = { ' ', '_', '=', '+', ',' };
+            // 加上后面枚举参数， 分割成多个字符串
+            string[] s1 = s.Split(chs,StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine(s1[i]);
+            }
+            
+            Console.ReadKey();
+            
+        }
+
+        public static void Test2()
+        {
+            StringBuilder sb = new StringBuilder();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100000; i++)
+            {
+                sb.Append(i);
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            //Console.WriteLine(sb.ToString());
+            Console.ReadKey();
+        }
+
+        public static void Test1()
+        {
+            string str = null;
+            // 创建了一个计时器， 用来记录程序运行的时间
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for(int i = 0; i < 100000; i++)
+            {
+                str += i;
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 05Day/_值类型和引用类型/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _值类型和引用类型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //_值类型和引用类型
+            //1.值类型和引用类型在内存上存储的地方不一样
+            //2.在传递值类型和传递引用累心个到时候，传递的方式不一样。
+            //  值类型我们称之为值传递， 引用类型我们称之为引用传递
+            //  值类型的值是存储在内存的栈当中， 引用类型的值存储在内存的堆当中
+
+            StringTest2();
+        }
+
+        // 字符串的不可变性
+        public static void StringTest()
+        {
+            string str1 = "张三";
+            string str2 = "张三";
+            // 即时窗口下 &str1 &str2 分别查看两个字符串存储的地址相同
+            //&str1
+            //0x0034ed08
+            //    * &str1: 0x023b23f0
+            //& str2
+            //0x0034ed04
+            //    * &str2: 0x023b23f0
+            Console.ReadKey();
+        }
+
+        // 字符串的不可变性
+        public static void StringTest2()
+        {
+            string str1 = "张三";
+            string str2 = "张三";
+
+            str1 = "李四";
+            Console.ReadKey();
+        }
+
+    }
+}
