1f148f791b316fae12b476e084d4f0cf125ce0e9
Ryan Shen <shenyj4@51nb.com>
2017-12-12 22:00:21 +0800

    ComboBox

---
+++ Winform开发/ComboBox控件/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ComboBox控件
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboYear = new System.Windows.Forms.ComboBox();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.cboDays = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboYear
+            // 
+            this.cboYear.FormattingEnabled = true;
+            this.cboYear.Location = new System.Drawing.Point(38, 36);
+            this.cboYear.Name = "cboYear";
+            this.cboYear.Size = new System.Drawing.Size(121, 20);
+            this.cboYear.TabIndex = 0;
+            this.cboYear.SelectedIndexChanged += new System.EventHandler(this.cboYear_SelectedIndexChanged);
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Location = new System.Drawing.Point(240, 35);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(121, 20);
+            this.cboMonth.TabIndex = 1;
+            this.cboMonth.SelectedIndexChanged += new System.EventHandler(this.cboMonth_SelectedIndexChanged);
+            // 
+            // cboDays
+            // 
+            this.cboDays.FormattingEnabled = true;
+            this.cboDays.Location = new System.Drawing.Point(453, 35);
+            this.cboDays.Name = "cboDays";
+            this.cboDays.Size = new System.Drawing.Size(121, 20);
+            this.cboDays.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(639, 316);
+            this.Controls.Add(this.cboDays);
+            this.Controls.Add(this.cboMonth);
+            this.Controls.Add(this.cboYear);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cboYear;
+        private System.Windows.Forms.ComboBox cboMonth;
+        private System.Windows.Forms.ComboBox cboDays;
+    }
+}
+
---
+++ Winform开发/ComboBox控件/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox控件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0 && !comboBox1.Items.Contains(textBox1.Text.Length))
+            {
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+                MessageBox.Show("添加成功");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int currentYear = DateTime.Now.Year;
+            for (int i = currentYear; i >= 1949; i--)
+            {
+                cboYear.Items.Add(i + "年");
+            }
+        }
+
+        private void cboYear_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboMonth.Items.Count > 0) return;
+            for (int i = 1; i <= 12; i++)
+            {
+                cboMonth.Items.Add(i + "月");
+            }
+        }
+
+        private void cboMonth_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cboDays.Items.Clear();
+            cboDays.ResetText();
+            int currentMonthDays = 0;
+            string currentYear = cboYear.SelectedItem.ToString().Split(new char[] { '年' }, StringSplitOptions.RemoveEmptyEntries)[0];
+            string currentMonth = cboMonth.SelectedItem.ToString().Split(new char[] { '月'},  StringSplitOptions.RemoveEmptyEntries)[0];
+            int year = int.Parse(currentYear);
+            int month = int.Parse(currentMonth);
+
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    currentMonthDays = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    currentMonthDays = 30;
+                    break;
+                default:
+                    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                    {
+                        currentMonthDays = 29;
+                    }
+                    else
+                    {
+                        currentMonthDays = 28;
+                    }
+                    break;
+            }
+
+            for (int i = 1; i <= currentMonthDays; i++)
+            {
+                cboDays.Items.Add(i + "日");
+            }
+        }
+    }
+}
