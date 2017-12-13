04315fcf3ad516a6363a28956cff4d08c5632fbf
Ryan Shen <shenyj4@51nb.com>
2017-12-13 16:58:14 +0800

    小练习记事本

---
+++ Winform开发/_记事本应用程序/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace _记事本应用程序
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.格式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.样式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开记录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.隐藏ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自动换行ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.格式ToolStripMenuItem,
+            this.样式ToolStripMenuItem,
+            this.打开记录ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(488, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemOpen,
+            this.保存ToolStripMenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 格式ToolStripMenuItem
+            // 
+            this.格式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.自动换行ToolStripMenuItem});
+            this.格式ToolStripMenuItem.Name = "格式ToolStripMenuItem";
+            this.格式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.格式ToolStripMenuItem.Text = "格式";
+            // 
+            // 样式ToolStripMenuItem
+            // 
+            this.样式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemFont,
+            this.ToolStripMenuItemColor});
+            this.样式ToolStripMenuItem.Name = "样式ToolStripMenuItem";
+            this.样式ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.样式ToolStripMenuItem.Text = "样式";
+            // 
+            // 打开记录ToolStripMenuItem
+            // 
+            this.打开记录ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.隐藏ToolStripMenuItem});
+            this.打开记录ToolStripMenuItem.Name = "打开记录ToolStripMenuItem";
+            this.打开记录ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.打开记录ToolStripMenuItem.Text = "打开记录";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(0, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(488, 323);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 323);
+            this.panel1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 328);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.显示ToolStripMenuItem.Text = "显示";
+            this.显示ToolStripMenuItem.Click += new System.EventHandler(this.显示ToolStripMenuItem_Click);
+            // 
+            // 隐藏ToolStripMenuItem
+            // 
+            this.隐藏ToolStripMenuItem.Name = "隐藏ToolStripMenuItem";
+            this.隐藏ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.隐藏ToolStripMenuItem.Text = "隐藏";
+            this.隐藏ToolStripMenuItem.Click += new System.EventHandler(this.隐藏ToolStripMenuItem_Click);
+            // 
+            // ToolStripMenuItemOpen
+            // 
+            this.ToolStripMenuItemOpen.Name = "ToolStripMenuItemOpen";
+            this.ToolStripMenuItemOpen.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemOpen.Text = "打开";
+            this.ToolStripMenuItemOpen.Click += new System.EventHandler(this.ToolStripMenuItemOpen_Click);
+            // 
+            // 保存ToolStripMenuItem
+            // 
+            this.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem";
+            this.保存ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.保存ToolStripMenuItem.Text = "保存";
+            this.保存ToolStripMenuItem.Click += new System.EventHandler(this.保存ToolStripMenuItem_Click);
+            // 
+            // 自动换行ToolStripMenuItem
+            // 
+            this.自动换行ToolStripMenuItem.Name = "自动换行ToolStripMenuItem";
+            this.自动换行ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.自动换行ToolStripMenuItem.Text = "自动换行";
+            this.自动换行ToolStripMenuItem.Click += new System.EventHandler(this.自动换行ToolStripMenuItem_Click);
+            // 
+            // ToolStripMenuItemFont
+            // 
+            this.ToolStripMenuItemFont.Name = "ToolStripMenuItemFont";
+            this.ToolStripMenuItemFont.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemFont.Text = "字体";
+            this.ToolStripMenuItemFont.Click += new System.EventHandler(this.ToolStripMenuItemFont_Click);
+            // 
+            // ToolStripMenuItemColor
+            // 
+            this.ToolStripMenuItemColor.Name = "ToolStripMenuItemColor";
+            this.ToolStripMenuItemColor.Size = new System.Drawing.Size(152, 22);
+            this.ToolStripMenuItemColor.Text = "颜色";
+            this.ToolStripMenuItemColor.Click += new System.EventHandler(this.ToolStripMenuItemColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 350);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 格式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 样式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开记录ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 隐藏ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemOpen;
+        private System.Windows.Forms.ToolStripMenuItem 保存ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 自动换行ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemFont;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemColor;
+    }
+}
+
---
+++ Winform开发/_记事本应用程序/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _记事本应用程序
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> FullFilePathsList = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void 显示ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+        }
+
+        private void 隐藏ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void ToolStripMenuItemOpen_Click(object sender, EventArgs e)
+        {
+            // 打开对话框
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "选择要打开的目录";
+            openDialog.ShowDialog();
+            string path = Path.GetFileName(openDialog.FileName);
+            try
+            {
+                if (!listBox1.Items.Contains(path))
+                {
+                    listBox1.Items.Add(path);
+                    FullFilePathsList.Add(openDialog.FileName);
+                    try
+                    {
+                        using (FileStream readFileStream = new FileStream(openDialog.FileName, FileMode.OpenOrCreate, FileAccess.Read))
+                        {
+                            int length = 0;
+                            while (true)
+                            {
+                                byte[] buffer = new byte[1024 * 1024];
+                                length = readFileStream.Read(buffer, 0, buffer.Length);
+                                if (length == 0) break;
+                                string content = Encoding.Default.GetString(buffer, 0, length);
+                                textBox1.Text += content;
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("打开文件失败" + exp.ToString());
+            }
+
+        }
+
+        private void 保存ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Title = "请选择要保存的路径";
+            saveDialog.Filter = "文本文件|*.txt";
+
+            saveDialog.ShowDialog();
+
+            string savePath = saveDialog.FileName;
+
+            try
+            {
+                using (FileStream writeStream = new FileStream(savePath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(textBox1.Text);
+                    writeStream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("保存失败" + exp.ToString());
+            }
+
+        }
+
+        private void 自动换行ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ( 自动换行ToolStripMenuItem.Text == "√自动换行" )
+            {
+                textBox1.WordWrap = false;
+                自动换行ToolStripMenuItem.Text = "自动换行";
+            }
+            else
+            {
+                textBox1.WordWrap = true;
+                自动换行ToolStripMenuItem.Text = "√自动换行";
+            }
+        }
+
+        private void ToolStripMenuItemFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDiaLog = new FontDialog();
+            fontDiaLog.ShowDialog();
+            textBox1.Font = fontDiaLog.Font;
+        }
+
+        private void ToolStripMenuItemColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.ShowDialog();
+            textBox1.ForeColor = colorDialog.Color;
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            string fileFullPath = FullFilePathsList[index];
+            textBox1.Clear();
+            try
+            {
+                using (FileStream readStream = new FileStream(fileFullPath, FileMode.OpenOrCreate, FileAccess.Read))
+                {
+                    int length = 0;
+                    byte[] buffer = new byte[1024 * 1024];
+                    while (true)
+                    {
+                        length = readStream.Read(buffer, 0, buffer.Length);
+                        if (length == 0) break;
+                        string content = Encoding.Default.GetString(buffer);
+                        textBox1.Text += content;
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("打开失败" + exp.ToString());
+            }
+
+        }
+    }
+}
