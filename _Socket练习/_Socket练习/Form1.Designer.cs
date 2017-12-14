df3157eb17c84bdfed4ca29d988ece1ddb8b40d8
Ryan Shen <shenyj4@51nb.com>
2017-12-14 16:43:12 +0800

    Socket服务端接收数据

---
+++ _Socket练习/_Socket练习/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace _Socket练习
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.Location = new System.Drawing.Point(312, 21);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(75, 23);
+            this.btnWatch.TabIndex = 0;
+            this.btnWatch.Text = "开启监听";
+            this.btnWatch.UseVisualStyleBackColor = true;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 61);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 96);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tbIp
+            // 
+            this.tbIp.Location = new System.Drawing.Point(93, 24);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(100, 21);
+            this.tbIp.TabIndex = 2;
+            this.tbIp.Text = "192.168.0.112";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(34, 27);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(53, 12);
+            this.lblIP.TabIndex = 3;
+            this.lblIP.Text = "本机IP：";
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(248, 23);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(43, 21);
+            this.tbPort.TabIndex = 4;
+            this.tbPort.Text = "50000";
+            this.tbPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(199, 27);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(53, 12);
+            this.lblPort.TabIndex = 5;
+            this.lblPort.Text = "端口号：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 262);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnWatch);
+            this.MaximumSize = new System.Drawing.Size(450, 300);
+            this.MinimumSize = new System.Drawing.Size(450, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWatch;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label lblPort;
+    }
+}
+
---
+++ _Socket练习/_Socket练习/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _Socket练习
+{
+    public partial class Form1 : Form
+    {
+        // 线程
+        private Thread thread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            // 在服务端创建一个负责监听IP地址跟端口号的Socket
+            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.IP);
+            IPAddress ip = IPAddress.Any;//IPAddress.Parse(tbIp.Text);
+            // 创建端口号对象 192.168.0.112
+            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(tbPort.Text));
+            // 监听
+            socketWatch.Bind(point);
+            ShowMsg("监听成功");
+            socketWatch.Listen(10);
+
+            thread = new Thread(WaitToConnectSocket);
+            thread.IsBackground = true;
+            thread.Start(socketWatch);
+        }
+
+        private void WaitToConnectSocket(Object obj)
+        {
+            if (obj is Socket)
+            {
+                Socket socketWatch = obj as Socket;
+                while (true)
+                {
+                    // 等待客户端的连接， 并创建一个负责通信的Socket
+                    Socket socketSend = socketWatch.Accept();
+                    ShowMsg(socketSend.RemoteEndPoint + ":连接成功");
+                    // 创建一个线程去接收数据
+                    Thread receivedMsgThread = new Thread(ReceivedMessage);
+                    receivedMsgThread.IsBackground = true;
+                    receivedMsgThread.Start(socketSend);
+                }
+            }
+        }
+        /// <summary>
+        /// 服务端不断的接收客户端发送来的数据
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ReceivedMessage(Object obj)
+        {
+            Socket socketSend = obj as Socket;
+            bool flag = true;
+            while (flag)
+            {
+                // 客户端连接成功后， 服务器接收客户端发来的消息
+                byte[] buffer = new byte[1024 * 1024 * 2];
+                int length = socketSend.Receive(buffer);
+                if (length == 0)
+                {
+                    flag = false;
+                    break;
+                }
+                // 将接收到的数据转成字符串
+                string content = Encoding.Default.GetString(buffer, 0, length);
+                ShowMsg(socketSend.RemoteEndPoint + ":" + content);
+            }
+        }
+
+        private void ShowMsg(string msg)
+        {
+            textBox1.AppendText(msg + "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            thread.Abort();
+        }
+    }
+}
