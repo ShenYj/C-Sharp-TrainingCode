fc830e1a8e0ca294344e955cd2bdcb463f3f4d41
Ryan Shen <shenyj4@51nb.com>
2017-12-18 12:30:17 +0800

    C# 的单例

---
+++ 11Day/_单例/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _单例
+{
+    public partial class Form2 : Form
+    {
+        // 创建一个单例
+        public static Form2 _formSingle = null;
+        public static Form2 Shared()
+        {
+            if (_formSingle == null)
+            {
+                _formSingle = new Form2();
+            }
+            return _formSingle;
+        }
+
+        private Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
