19b980cec4de8290251faadf82d22b79b09a26e4
Ryan Shen <shenyj4@51nb.com>
2018-01-12 17:42:22 +0800

    标示枚举

---
+++ Day13_WPF/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 枚举
+            Des d = Des.gao | Des.fu;
+            Console.WriteLine(d);
+            Console.ReadKey();
+            
+        }
+        [Flags]// 标示枚举
+        public enum Des
+        {
+            gao   = 1,
+            fu    = 2,
+            shuai = 4,
+        }
+    }
+}
