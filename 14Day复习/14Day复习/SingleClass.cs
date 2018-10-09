21e92420ded8df5bbbef39b7875ea467639b2044
ShenYj <shenyj@51nb.com>
2018-10-09 08:08:35 +0800

    14Day

---
+++ 14Day复习/14Day复习/SingleClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14Day复习
+{
+    // 单例练习
+    class SingleClass
+    {
+
+        public static SingleClass _singleClass = null;
+
+        private SingleClass()
+        {
+        }
+
+        public static SingleClass Shared()
+        {
+            if (_singleClass != null) return _singleClass;
+            _singleClass = new SingleClass();
+            return _singleClass;
+        }
+    }
+}
