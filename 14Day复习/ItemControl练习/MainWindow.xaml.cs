8cb2184456f055b52c4eecf338c6930e7d266aae
ShenYj <shenyj@51nb.com>
2018-10-16 11:50:46 +0800

    ItemsControl

---
+++ 14Day复习/ItemControl练习/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItemControl练习
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = this;
+            Friends = new ObservableCollection<Person>();
+            Friends.Add(new Person() { Name = "小明", Gender = "男" });
+            Friends.Add(new Person() { Name = "小红", Gender = "女" });
+            Friends.Add(new Person() { Name = "小航", Gender = "男" });
+            Friends.Add(new Person() { Name = "小黑", Gender = "男" });
+            Friends.Add(new Person() { Name = "小A", Gender = "女" });
+            Friends.Add(new Person() { Name = "小甲", Gender = "男" });
+            Friends.Add(new Person() { Name = "小鸡", Gender = "女" });
+        }
+
+        public ObservableCollection<Person> Friends
+        {
+            get
+            {
+                return _friends;
+            }
+            set
+            {
+                _friends = value;
+                RaisePropertyChanged("Friends");
+            }
+        }
+
+        private ObservableCollection<Person> _friends { get; set; }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+        #region Methods
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        public String Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                _gender = value;
+                RaisePropertyChanged("Gender");
+            }
+        }
+        private String _gender { get; set; }
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+        private String _name { get; set; }
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+        #region Methods
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
--- 14Day复习/ListBox练习/MainWindow.xaml.cs
+++ 14Day复习/ListBox练习/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,33 @@ namespace ListBox练习
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MyListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            //Hashtable ht1 = new Hashtable();
+            //ht1.Add("account", "CB2342432432");
+            //ht1.Add("cacheDate", "2017/08/18-2018/03/13");
+            //ht1.Add("cacheDuration", "10天7时");
+            //ht1.Add("phneNumber", "13888888888");
+            //ht1.Add("gender", "女");
+            //ht1.Add("MAC", "10:20:30:AC:ED");
+            //ht1.Add("account", "CB2342432432");
+            //ht1.Add("name", "张三");
+            //ht1.Add("upload", "未上传");
+            //ht1.Add("bind", "绑定用户");
+            //ht1.Add("apply", "申请");
+
+            DeviceListItem item = new DeviceListItem();
+            item.DAccount.Content = "CB2342432432";
+            item.DCacheDate.Content = "2017/08/18-2018/03/13";
+            item.DCacheDuration.Content = "10天7时";
+            item.DName.Content = "张三";
+            item.DPhoneNumber.Content = "13888888888";
+            item.DGender.Content = "女";
+            item.DIsBinding.IsEnabled = false;
+            MyListBox.Items.Add(item);
         }
     }
 }
